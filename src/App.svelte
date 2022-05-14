d283f08098bb902c5375407b3d5dc26cc40a9cc3
Vu Doan <vu@voly.co.uk>
2022-05-14 11:21:16 +0100

    VU-2: GETTING STARTED ON REPLICATING THE UI

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import TextEditor from "./lib/TextEditor.svelte";
+</script>
+
+<main>
+  <section id="wrap">
+    <TextEditor />
+  </section>
+</main>
+
+<style>
+  #wrap {
+    position: relative;
+    margin: 0 auto 0 auto;
+    padding: 50px 0 0 0;
+    width: 800px;
+  }
+</style>
---
+++ src/lib/TextEditor.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+</script>
+
+<!-- <template> -->
+  <form action="#" id="textEditor">
+    <div id="editorWrapper">
+      <textarea id="editor" wrap="soft"></textarea>
+    </div>
+    <div id="preview" style="display: none;"></div>
+  </form>
+<!-- </template> -->
+
+<style>
+  form#textEditor {
+    position: relative;
+    margin: 0;
+    padding: 0;
+    height: 600px;
+  }
+
+  #editorWrapper {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    padding: var(--padding) 0;
+    color: var(--color-primary);
+    background-color: var(--color-secondary);
+    border-top: 1px solid #010;
+    border-bottom: 1px solid #010;
+    overflow: visible;
+    font-size: 12pt;
+  }
+
+  #editor,
+  #preview {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box;
+    color: var(--color-primary);
+    background-color: var(--color-secondary);
+    border: 0;
+    overflow-y: scroll;
+    overflow-x: hidden;
+    outline: none;
+    resize: none;
+    word-wrap: break-word;
+    white-space: pre-wrap;
+    font-feature-settings: "liga" 0;
+  }
+
+  #editor {
+    color: var(--color-primary);
+    background-color: var(--color-secondary);
+    line-height: 120%;
+    font-family: monospace, monospace;
+    font-size: 100%;
+  }
+</style>
