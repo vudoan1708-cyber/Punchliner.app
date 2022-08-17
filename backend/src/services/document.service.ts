acc3a88ce00a1b4f84d156776ec08b123599c1b1
tranqhuy46 <tranquochuy4698@gmail.com>
2022-08-17 21:46:59 +0700

    hotfix: count document

--- backend/src/services/document.service.ts
+++ backend/src/services/document.service.ts
@@ -1,8 +1,8 @@
 import { load } from "cheerio";
 
 function countContentWords(content?: string): number {
-  if (!content) return 1;
-  const refinedContent = content.replaceAll(/\\/g, "");
+  if (content == null) return 1;
+  const refinedContent = content?.replaceAll(/\\/g, "");
   const $ = load(refinedContent);
   const innerText = $("body").text().replaceAll(/\s/g, "");
   return innerText.length ?? 1;
