2748339891333c53ca77ab15c4cde6baa92e96e2
Vu Doan <vu@voly.co.uk>
2022-08-22 00:05:05 +0100

    VU-12: WORKING ON VIEW SHARED DOC AS A GUEST

--- frontend/src/views/Shared.svelte
+++ frontend/src/views/Shared.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { onMount, afterUpdate } from 'svelte';
+  import { navigate } from 'svelte-navigator';
 
   import Loading from '../components/Loading.svelte';
 
@@ -13,7 +14,15 @@
   // Utility
   import { cookiestore } from '../helper/storage';
 
+  import punchlinerappLogo from '../assets/punchlinerappLogo.png';
+
   export let documentId: string = null;
+
+  let passcode: string = null;
+
+  const goToHomepage = (): void => {
+    navigate('/');
+  };
 
   const error: Error = {
     message: '',
@@ -24,19 +33,26 @@
   let loading: boolean = false;
   onMount(async () => {
     loading = true;
-    const sessionId = cookiestore.get('session');
-    const res = await DocumentQueryBuilder().addDocumentId(documentId).GET(sessionId);
-    if (!res.success) {
-      error.message = res.message;
-      error.detail = res.detail;
-      return;
+    try {
+      const sessionId = cookiestore.get('session');
+      const userId = cookiestore.get('userId');
+      const res = await DocumentQueryBuilder().addDocumentId(documentId).GET(sessionId);
+      if (!res.success) {
+        error.message = res.message;
+        error.detail = res.detail;
+        return;
+      }
+      document = res.data.document;
+    } catch (err) {
+      error.message = err.message;
+      error.detail = err.detail;
+    } finally {
+      loading = false;
     }
-    loading = false;
-    document = res.data.document;
   });
 
   afterUpdate(() => {
-    if (!!sharedRef && sharedRef.childNodes.length > 2) {
+    if (!!sharedRef) {
       const nodes = sharedRef.querySelectorAll('.hide');
       nodes.forEach((node) => {
         node.classList.replace('hide', 'shared-hide');
@@ -47,6 +63,10 @@
 
 <!-- <template> -->
   <div id="shared" bind:this={sharedRef}>
+    <div class="appLogo" title="Back to Homepage" on:click={goToHomepage}>
+      <img src={punchlinerappLogo} alt="app logo" />
+      <h2>unchliner app</h2>
+    </div>
     {#if !!document}
       <h2>{document.title}</h2>
       <div>{@html document.content}</div>
@@ -64,5 +84,29 @@
     background-color: var(--color-primary);
     color: transparent;
     cursor: not-allowed;
+  }
+
+  h2 {
+    margin-bottom: var(--margin);
+  }
+
+  .appLogo {
+    position: relative;
+    width: fit-content;
+    display: flex;
+    padding: var(--padding);
+    border-radius: var(--border-radius);
+    background-color: var(--color-neutral-800);
+    margin-bottom: calc(var(--margin) * 4);
+    cursor: pointer;
+  }
+
+  .appLogo:hover {
+    filter: grayscale(20%);
+  }
+
+  .appLogo img {
+    position: relative;
+    width: 50px;
   }
 </style>
