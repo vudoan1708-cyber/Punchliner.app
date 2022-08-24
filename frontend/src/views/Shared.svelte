78298f8f3b3cdf58a28d75bdf6bafc5961497190
Vu Doan <vu@voly.co.uk>
2022-08-24 08:49:23 +0100

    VU-12: INJECT MODAL FOR PASSCODE OUTSIDE THE CONTAINER DIV

--- frontend/src/views/Shared.svelte
+++ frontend/src/views/Shared.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { onMount, afterUpdate } from 'svelte';
+  import { onMount } from 'svelte';
   import { navigate } from 'svelte-navigator';
 
   import Loading from '../components/Loading.svelte';
@@ -88,15 +88,6 @@
     viewSharedDoc();
   });
 
-  afterUpdate(() => {
-    if (!!sharedRef) {
-      const nodes = sharedRef.querySelectorAll('.hide');
-      nodes.forEach((node) => {
-        node.classList.replace('hide', 'shared-hide');
-      });
-    }
-  });
-
   $: if (!!passcodeRef) {
       passcodeRef.focus();
     }
@@ -116,52 +107,46 @@
     {:else}
       No Content to display
     {/if}
-
-    <!-- Modal for passcode -->
-    {#if (!!error.message || !!error.detail) && !passcode.required}
-      <Modal title="Message" style="min-height: 3em;" hideClose>
-        <p style="margin: var(--margin) 0;">{error.message}</p>
-      </Modal>
-    {:else if passcode.required}
-      <Modal title="Message" style="min-height: 7em;" hideClose>
-        <p style="font-size: calc(var(--type-body-size));">This document requires a passcode before it can be revealed to you.
-          Please enter the passcode that is shared to you from the document's owner</p>
-        <form class="passcodeForm" on:submit|preventDefault>
-          <label for="passcode">
-            Passcode
-            <input type="password" minlength="6" bind:value={passcode.code} bind:this={passcodeRef} />
-          </label>
-
-          <Button
-            style={`height: fit-content; align-self: flex-end; display: flex;
-              align-items: center;
-              gap: calc(var(--margin) / 2);`}
-            disabled={!passcode.code}
-            on:click={() => {
-              if (passcode.code.length < 6) return;
-              viewSharedDoc();
-            }}>
-              <Icon name="check" />
-              OK
-          </Button>
-        </form>
-
-        {#if (!!error.message || !!error.detail) && passcode.times > 1}
-          <p class="inlineErrorMes">{error.message}</p>
-        {/if}
-      </Modal>
-    {/if}
   </div>
+
+  <!-- Modal for other error message -->
+  {#if (!!error.message || !!error.detail) && !passcode.required}
+    <Modal title="Message" style="min-height: 3em;" hideClose>
+      <p style="margin: var(--margin) 0;">{error.message}</p>
+    </Modal>
+  <!-- Modal for passcode -->
+  {:else if passcode.required}
+    <Modal title="Message" style="min-height: 7em;" hideClose>
+      <p style="font-size: calc(var(--type-body-size));">This document requires a passcode before it can be revealed to you.
+        Please enter the passcode that is shared to you from the document's owner</p>
+      <form class="passcodeForm" on:submit|preventDefault>
+        <label for="passcode">
+          Passcode
+          <input type="password" minlength="6" bind:value={passcode.code} bind:this={passcodeRef} />
+        </label>
+
+        <Button
+          style={`height: fit-content; align-self: flex-end; display: flex;
+            align-items: center;
+            gap: calc(var(--margin) / 2);`}
+          disabled={!passcode.code}
+          on:click={() => {
+            if (passcode.code.length < 6) return;
+            viewSharedDoc();
+          }}>
+            <Icon name="check" />
+            OK
+        </Button>
+      </form>
+
+      {#if (!!error.message || !!error.detail) && passcode.times > 1}
+        <p class="inlineErrorMes">{error.message}</p>
+      {/if}
+    </Modal>
+  {/if}
 <!-- </template> -->
 
 <style scoped>
-  :global(.shared-hide) {
-    filter: blur(2px);
-    background-color: var(--color-primary);
-    color: transparent;
-    cursor: not-allowed;
-  }
-
   #shared.blured {
     filter: blur(5px);
   }
