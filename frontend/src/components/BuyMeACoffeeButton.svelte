bf8ac743c39b634bd352ab7e6586b714272a48b5
Vu Doan <vu@voly.co.uk>
2022-09-29 23:40:09 +0100

    BUYMEACOFFEE BUTTON COMPONENT CREATED

---
+++ frontend/src/components/BuyMeACoffeeButton.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Button from "./Button.svelte";
+  import Icon from "./Icon/Icon.svelte";
+</script>
+
+<!-- <template> -->
+  <Button href="https://www.buymeacoffee.com/punchlinerapp" style="background-color: #ff8800; border-color: #ff8800;">
+    <div class="btn_wrapper">
+      <Icon name="buymeacoffee" />
+      Buy me a coffee
+    </div>
+  </Button>
+<!-- </template> -->
+
+<style>
+  .btn_wrapper {
+    display: flex;
+    align-items: center;
+    gap: calc(var(--margin) / 2);
+  }
+</style>
--- frontend/src/helper/constants.ts
+++ frontend/src/helper/constants.ts
@@ -3,9 +3,9 @@ import { isDev } from './utilities';
 import { PLACEHOLDER } from './utility_variables/text';
 import { BROWSER } from './utility_variables/browsers';
 
-export const ACCOUNTAPI = isDev() ? 'http://localhost:3333/v1/auth' : 'https://www.punchliner.app/api/v1/auth';
-export const DAPI = isDev() ? 'http://localhost:3333/v1/document' : 'https://www.punchliner.app/api/v1/document';
-export const PAPI = isDev() ? 'http://localhost:3333/v1/payment' : 'https://www.punchliner.app/api/v1/payment';
+export const ACCOUNTAPI = isDev() ? 'http://localhost:3333/api/v2/auth' : 'https://www.punchliner.app/api/v2/auth';
+export const DAPI = isDev() ? 'http://localhost:3333/api/v2/document' : 'https://www.punchliner.app/api/v2/document';
+export const PAPI = isDev() ? 'http://localhost:3333/api/v2/payment' : 'https://www.punchliner.app/api/v2/payment';
 
 export const USER_TYPE = {
   PREMIUM: 'PREMIUM',
